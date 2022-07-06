f28b4880987297be40d7c81a984e93f89f7baf07
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-07-06 15:00:29 -0300

    mudancas nas importacoes

--- banco.java
+++ banco.java
@@ -1,8 +1,11 @@
 package Banco;
 
+import java.util.*;
+
 public class Banco {
 
     private String nome;
+    private List<Conta> contas;
 
     public String getNome() {
         return nome;
@@ -10,5 +13,11 @@ public class Banco {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    public List<Conta> getContas() {
+        return contas;
+    }
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
     }
 }
